9eed47b98503dae4daacd6e7086c8510bbc09a50
Volodymyr Tarasenko <tvntsr@yahoo.com>
2022-10-10 03:16:43 +0300

    WorkerPool tests are added

--- workerpool_test.go
+++ workerpool_test.go
@@ -2,26 +2,29 @@ package pool
 
 import (
 	"testing"
+	"time"
 )
 
 func Test_NewWorkerPool(t *testing.T) {
-
 	pool_size := 10
 
+	var pool Pool
 	pool, err := NewWorkerPool(pool_size)
 	if err != nil {
 		t.Fatalf("Error %v", err)
 	}
 
-	if pool.max_size != pool_size {
+	wpool := pool.(*WorkerPool)
+
+	if wpool.max_size != pool_size {
 		t.Fatal("Wrong pool size")
 	}
 
-	if pool.tasks != nil {
+	if wpool.tasks != nil {
 		t.Fatal("Wrong pool tasks list")
 	}
 
-	if pool.available != nil {
+	if wpool.available != nil {
 		t.Fatal("Wrong pool available list")
 	}
 }
@@ -74,5 +77,108 @@ func Test_WorkerPoolStop(t *testing.T) {
 
 	if len(pool.available) != 0 {
 		t.Fatalf("Wrong pool available list, %d", len(pool.available))
+	}
+}
+
+func Test_WorkerPoolPushTask(t *testing.T) {
+	pool_size := 2
+
+	pool, err := NewWorkerPool(pool_size)
+	if err != nil {
+		t.Fatalf("Pool creating error %v", err)
+	}
+
+	err = pool.Start()
+	if err != nil {
+		t.Fatalf("Pool start error %v", err)
+	}
+
+	worker, err := pool.PushTask(func() (interface{}, error) {
+		return 1, nil
+	})
+
+	if err != nil {
+		t.Fatalf("Pool push task error %v", err)
+	}
+	if worker == nil {
+		t.Fatal("Worker is nil!")
+		t.FailNow()
+	}
+
+	ret, err := worker.WaitFinished()
+	if err != nil {
+		t.Fatalf("WaitFinished failed %v", err)
+	}
+	if ret != 1 {
+		t.Fatalf("Wrong value returned, %v", ret)
+	}
+}
+
+func Test_WorkerPoolPushLongTask(t *testing.T) {
+	pool_size := 2
+
+	pool, err := NewWorkerPool(pool_size)
+	if err != nil {
+		t.Fatalf("Pool creating error %v", err)
+	}
+
+	err = pool.Start()
+	if err != nil {
+		t.Fatalf("Pool start error %v", err)
+	}
+
+	worker, err := pool.PushTask(func() (interface{}, error) {
+		time.Sleep(3 * time.Second)
+		return 1, nil
+	})
+
+	if err != nil {
+		t.Fatalf("Pool push task error %v", err)
+	}
+	if worker == nil {
+		t.Fatal("Worker is nil!")
+		t.FailNow()
+	}
+
+	ret, err := worker.WaitFinished()
+	if err != nil {
+		t.Fatalf("WaitFinished failed %v", err)
+	}
+	if ret != 1 {
+		t.Fatalf("Wrong value returned, %v", ret)
+	}
+}
+
+func Test_WorkerPoolPushTooMuchTasks(t *testing.T) {
+	pool_size := 2
+
+	pool, err := NewWorkerPool(pool_size)
+	if err != nil {
+		t.Fatalf("Pool creating error %v", err)
+	}
+
+	err = pool.Start()
+	if err != nil {
+		t.Fatalf("Pool start error %v", err)
+	}
+
+	for i := 0; i < pool_size+2; i++ {
+		worker, err := pool.PushTask(func() (interface{}, error) {
+			time.Sleep(3 * time.Second)
+			return 1, nil
+		})
+
+		if err == nil && i > pool_size {
+			t.Fatalf("Pool pushed too much tasks, %d", i)
+		}
+
+		if err != nil && i < pool_size {
+			t.Fatalf("Pool push task error %v", err)
+			t.FailNow()
+		}
+		if worker == nil && i < pool_size {
+			t.Fatal("Worker is nil!")
+			t.FailNow()
+		}
 	}
 }
