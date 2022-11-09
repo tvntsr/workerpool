e4500a3f9bf4d2ac0c079292d53083896129e371
Volodymyr Tarasenko <tvntsr@users.noreply.github.com>
2022-11-09 22:42:44 +0200

    startTaskAt: initial implementation

--- scheduler.go
+++ scheduler.go
@@ -4,48 +4,56 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"sync"
 	"sync/atomic"
 	"time"
 )
 
-/*
-Usage:
-   pool.Start()
-   scheduler = NewScheduler(pool)
-
-   taskAt, err := scheduler.RunTaskAt(10s, job_function)
-   ...
-   taskAfter, err := scheduler.RunTaskAfter(taskAt, job_function_2)
-
-   taskBefore, err := scheduler.RunTaskBefore(taskAfter, job_function_3)
-
-   scheduler.Stop()
-   pool.Stop()
-*/
+// Usage:
+//    pool.Start()
+//    scheduler = NewScheduler(pool)
+//
+//    taskAt, err := scheduler.RunTaskAt(10s, job_function)
+//    ...
+//    taskAfter, err := scheduler.RunTaskAfter(taskAt, job_function_2)
+//
+//    taskBefore, err := scheduler.RunTaskBefore(taskAfter, job_function_3)
+//
+//    scheduler.Stop()
+//    pool.Stop()
 
 type JobFunction func() (interface{}, error)
 
+// ::TODO:: implement all required methods, WorkerTask is temporally only
+// ::TODO:: would it be better to extend WorkerTask?
 type SchedulerTask struct {
-	next *SchedulerTask
-	prev *SchedulerTask
+	WorkerTask
 
-	task Task
+	//	next *SchedulerTask
+	//	prev *SchedulerTask
+
+	task     Task
+	function JobFunction
+	ticker   *time.Ticker
 }
 
-func (sht *SchedulerTask) setTask(t Task) {
-	sht.task = t
+func (sht *SchedulerTask) startTaskAt(when time.Duration) <-chan time.Time {
+	sht.ticker = time.NewTicker(when)
+
+	return sht.ticker.C
 }
 
 type Scheduler struct {
-	ch    chan JobFunction
-	chs   []chan JobFunction
-	pool  Pool
-	ready atomic.Value
+	com         chan int
+	channels    []<-chan time.Time
+	tasks       []*SchedulerTask
+	pool        Pool
+	ready       atomic.Value
+	terminating *sync.WaitGroup
 }
 
-func poll[T any](chs []chan T, cmd chan JobFunction) (int, T, error) {
-	var zeroT T
-
+func poll[T any](chs []<-chan T, cmd chan int) (int, error) {
+	fmt.Printf("POLL: Startign poll for %d records\n", len(chs))
 	cases := make([]reflect.SelectCase, len(chs)+1)
 
 	for i, ch := range chs {
@@ -59,29 +67,39 @@ func poll[T any](chs []chan T, cmd chan JobFunction) (int, T, error) {
 
 	// ok will be true if the channel has not been closed.
 	chosen, value, ok := reflect.Select(cases)
+	fmt.Printf("POLL: Selected: chosen %v, value %v, ok %v\n", chosen, value, ok)
 	if !ok {
-		return chosen, zeroT, errors.New("channel closed")
-	}
-	if ret, ok := value.Interface().(T); ok {
-		return chosen, ret, nil
+		return chosen, errors.New("channel closed")
 	}
 
-	return chosen, zeroT, errors.New("failed to cast value")
+	return chosen, nil
 }
 
 func NewScheduler(p Pool) (*Scheduler, error) {
-	ch := make(chan JobFunction)
+	ch := make(chan int)
 
 	sch := &Scheduler{
-		ch:   ch,
-		chs:  make([]chan JobFunction, 0),
-		pool: p,
+		com:         ch,
+		channels:    make([]<-chan time.Time, 0),
+		tasks:       make([]*SchedulerTask, 0),
+		pool:        p,
+		terminating: &sync.WaitGroup{},
 	}
+	sch.terminating.Add(1)
+	starting := &sync.WaitGroup{}
+
+	starting.Add(1)
 
 	go func() {
+		starting.Done()
 		for {
-			selected, value, err := poll(sch.chs, sch.ch)
-			if selected == len(sch.chs)+1 {
+			fmt.Printf("POLLER: Wait poll, channels %d\n", len(sch.channels))
+			selected, err := poll(sch.channels, sch.com)
+			fmt.Printf("POLLER: Polled: selected %v, err %v\n", selected, err)
+			fmt.Printf("POLLER: Len: %v\n", len(sch.channels))
+
+			if selected == len(sch.channels) {
+				fmt.Println("POLLER: It is comm")
 				// ch channel, closed
 				if err != nil {
 					break // need to close all channels
@@ -91,55 +109,63 @@ func NewScheduler(p Pool) (*Scheduler, error) {
 				continue
 			}
 
-			// time to run value
+			//time to run value
 			if err == nil {
-				sch.pool.PushTask(value)
+				fmt.Println("POLLER: set task to execute")
+				scheduler_task := sch.tasks[selected]
+				scheduler_task.ticker.Stop()
+
+				// Delete: ::TODO:: should it be rewritten??
+				sch.tasks = append(sch.tasks[:selected], sch.tasks[selected+1:]...)
+				sch.channels = append(sch.channels[:selected], sch.channels[selected+1:]...)
+				t, err := sch.pool.PushTask(scheduler_task.function)
+				if err != nil {
+					// ::FIME:: handle error
+					fmt.Printf("ERROR: %v\n", err) // ::FIXME:: set error in the task
+				}
+				scheduler_task.task = t
+				// ::FIXME:: there is new task returned, map it
 			}
 		}
-		// close all channels
-		//close(ch) <- closed by stop
-		for _, c := range sch.chs {
-			close(c)
-		}
+		// close(ch) <- closed by stop
+
+		sch.terminating.Done()
+		fmt.Println("POLLER: Service func is quit")
 	}()
 
 	sch.ready.Store(true)
 
+	starting.Wait() // Make sure everything is started
+
+	fmt.Println("Scheduler: Init DONE")
+
 	return sch, nil
+}
+
+func (sc *Scheduler) RunTaskAt(at time.Duration, job func() (interface{}, error)) (Task, error) {
+	task := &SchedulerTask{}
+	task.function = job
+
+	task_channel := task.startTaskAt(at)
+
+	fmt.Println("Scheduler: Task's tiket created")
+	sc.channels = append(sc.channels, task_channel)
+	sc.tasks = append(sc.tasks, task)
+
+	fmt.Println("Scheduler: Notify poll")
+	sc.com <- len(sc.tasks)
+	fmt.Println("Scheduler: Notified")
+
+	return task, nil
 }
 
 func (sc *Scheduler) Stop() bool {
 	status := sc.ready.CompareAndSwap(true, false)
 	if status {
-		close(sc.ch)
+		close(sc.com)
+		sc.terminating.Wait()
 	}
 	return status
-}
-
-func (sc *Scheduler) RunTaskAt(at time.Duration, job func() (interface{}, error)) (Task, error) {
-	task := &SchedulerTask{}
-
-	f := func() (interface{}, error) {
-		ticker := time.NewTicker(at)
-		done := make(chan bool)
-		select {
-		case <-done:
-			ticker.Stop()
-			return nil, fmt.Errorf("Cancelled")
-		case <-ticker.C:
-			ticker.Stop()
-			return job()
-		}
-	}
-
-	t, err := sc.pool.PushTask(f)
-	if err != nil {
-		return nil, err
-	}
-
-	task.setTask(t)
-
-	return nil, fmt.Errorf("Not Implemented")
 }
 
 // func (sc *Scheduler) RunTaskBefore(before Task, job func() (interface{}, error)) (Task, error) {
--- scheduler_test.go
+++ scheduler_test.go
@@ -1,7 +1,9 @@
 package pool
 
 import (
+	"sync"
 	"testing"
+	"time"
 )
 
 func Test_NewScheduler(t *testing.T) {
@@ -59,4 +61,50 @@ func Test_SchedulerDoubleStop(t *testing.T) {
 	if status {
 		t.Fatal("Second stop() must fail")
 	}
+}
+
+func Test_SchedulerRunTaskAt(t *testing.T) {
+	pool, err := NewManagedPool(5)
+	if err != nil {
+		t.Fatalf("Pool creation failed, %v", err)
+	}
+
+	sc, err := NewScheduler(pool)
+	if err != nil {
+		t.Fatalf("Pool creation failed, %v", err)
+	}
+	err = pool.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	a := 1
+	r := &sync.WaitGroup{}
+	r.Add(1)
+	t.Log("Run method\n")
+	task, err := sc.RunTaskAt(1*time.Second,
+		func() (interface{}, error) {
+			t.Log("I am started\n")
+
+			a++
+			r.Done()
+
+			return nil, nil
+		})
+
+	if err != nil {
+		t.Errorf("Got error %v", err)
+	}
+	if task == nil {
+		t.Error("Task should not be nil")
+	}
+
+	r.Wait()
+
+	if a != 2 {
+		t.Error("Task was not executed")
+	}
+	_ = sc.Stop()
+	_ = pool.Stop()
+
 }
